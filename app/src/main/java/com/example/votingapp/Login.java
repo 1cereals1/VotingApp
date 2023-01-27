7767ceb94f9a4a8d91bc8b23db7a6a20a4d2a210
1cereals1 <92153295+1cereals1@users.noreply.github.com>
2023-01-27 16:52:53 +0800

    Firebase AUth for numbers and sht

--- app/src/main/java/com/example/votingapp/Login.java
+++ app/src/main/java/com/example/votingapp/Login.java
@@ -6,18 +6,41 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthOptions;
+import com.google.firebase.auth.PhoneAuthProvider;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.concurrent.TimeUnit;
+
 public class Login extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+
+    private EditText idnologin,edtPhone, edtOTP;
+
+    private Button loginb, generateOTPBtn, registerb;
+
+    // string for storing our verification ID
+    private String verificationId;
+
+
+
 
     DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://online-voting-ma-default-rtdb.firebaseio.com/");
     @Override
@@ -25,23 +48,44 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        idnologin = findViewById(R.id.IDLogin);
+        edtPhone = findViewById(R.id.idEdtPhoneNumber);
+        edtOTP = findViewById(R.id.idEdtOtp);
 
-        final EditText idnologin = findViewById(R.id.IDLogin);
-        final EditText passlogin = findViewById(R.id.Pass);
+        loginb = findViewById(R.id.LoginB);
+        generateOTPBtn = findViewById(R.id.idBtnGetOtp);
+        registerb = findViewById(R.id.Register);
 
-        final Button loginb = findViewById(R.id.LoginB);
-        final Button registerb = findViewById(R.id.Register);
 
+        // setting onclick listener for generate OTP button.
+        generateOTPBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // below line is for checking whether the user
+                // has entered his mobile number or not.
+                if (TextUtils.isEmpty(edtPhone.getText().toString())) {
+                    // when mobile number text field is empty
+                    // displaying a toast message.
+                    Toast.makeText(Login.this, "Please enter a valid phone number.", Toast.LENGTH_SHORT).show();
+                } else {
+                    // if the text field is not empty we are calling our
+                    // send OTP method for getting OTP from Firebase.
+                    String phone = edtPhone.getText().toString();
+                    sendVerificationCode(phone);
+                }
+            }
+        });
 
         loginb.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
                 final String idnologintext = idnologin.getText().toString();
-                final String passloginstext = passlogin.getText().toString();
+                final String edtPhonetext = edtPhone.getText().toString();
 
-                if(idnologintext.isEmpty() || passloginstext.isEmpty()){
-                    Toast.makeText(Login.this, "Fill the fields first", Toast.LENGTH_SHORT).show();
+
+                if(idnologintext.isEmpty()){
+                    Toast.makeText(Login.this, "Fill the field/s first", Toast.LENGTH_SHORT).show();
                 }
                 else {
 
@@ -49,19 +93,15 @@ public class Login extends AppCompatActivity {
                         @Override
                         public void onDataChange(@NonNull DataSnapshot snapshot) {
 
-                            //NOW to check if LastName is exists in our database
+                            //NOW to check if the ID used exists in our database
                             if (snapshot.hasChild(idnologintext)){
 
                                 //LastName exists in database..
-                                //NOW get 'Pass' from firebase data and match it with user entered Pass
-                                final String getpass = snapshot.child(idnologintext).child("Pass").getValue(String.class);
+                                //NOW get 'Child' from firebase data and match it with user entered 'Child'
+                                final String getnumber = snapshot.child(idnologintext).child("ContactNumber").getValue(String.class);
 
-                                if (getpass.equals(passloginstext)){
-                                    Toast.makeText(Login.this, "Logged in", Toast.LENGTH_SHORT).show();
-
-                                    //off to the main activity!!!
-                                    startActivity(new Intent(Login.this, AdminsEmployees.class));
-                                    finish();
+                                if (getnumber.equals(edtPhonetext)){
+                                    verifyCode(edtOTP.getText().toString());
                                 }
                                 else {
                                     Toast.makeText(Login.this, "WRONG DETAILS", Toast.LENGTH_SHORT).show();
@@ -93,8 +133,109 @@ public class Login extends AppCompatActivity {
 
 
     }
+
+
+    //Here we start with the phone number auth
+    private void signInWithCredential(PhoneAuthCredential credential) {
+        // inside this method we are checking if
+        // the code entered is correct or not.
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // if the code is correct and the task is successful
+                            // we are sending our user to new activity.
+                            Intent i = new Intent(Login.this, AdminsEmployees.class);
+                            startActivity(i);
+                            finish();
+                        } else {
+                            // if the code is not correct then we are
+                            // displaying an error message to the user.
+                            Toast.makeText(Login.this, task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+    }
+
     @Override
     public void onBackPressed() {
         Toast.makeText(getApplicationContext(), "You have exited application(But not really since this is a prototype)", Toast.LENGTH_SHORT).show();
     }
+
+    private void sendVerificationCode(String number) {
+        // this method is used for getting
+        // OTP on user phone number.
+        PhoneAuthOptions options =
+                PhoneAuthOptions.newBuilder(mAuth)
+                        .setPhoneNumber(number)            // Phone number to verify
+                        .setTimeout(60L, TimeUnit.SECONDS) // Timeout and unit
+                        .setActivity(this)                 // Activity (for callback binding)
+                        .setCallbacks(mCallBack)           // OnVerificationStateChangedCallbacks
+                        .build();
+        PhoneAuthProvider.verifyPhoneNumber(options);
+    }
+
+    // callback method is called on Phone auth provider.
+    private PhoneAuthProvider.OnVerificationStateChangedCallbacks
+
+            // initializing our callbacks for on
+            // verification callback method.
+            mCallBack = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+        // below method is used when
+        // OTP is sent from Firebase
+        @Override
+        public void onCodeSent(String s, PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+            super.onCodeSent(s, forceResendingToken);
+            // when we receive the OTP it
+            // contains a unique id which
+            // we are storing in our string
+            // which we have already created.
+            verificationId = s;
+        }
+
+        // this method is called when user
+        // receive OTP from Firebase.
+        @Override
+        public void onVerificationCompleted(PhoneAuthCredential phoneAuthCredential) {
+            // below line is used for getting OTP code
+            // which is sent in phone auth credentials.
+            final String code = phoneAuthCredential.getSmsCode();
+
+            // checking if the code
+            // is null or not.
+            if (code != null) {
+                // if the code is not null then
+                // we are setting that code to
+                // our OTP edittext field.
+                edtOTP.setText(code);
+
+                // after setting this code
+                // to OTP edittext field we
+                // are calling our verifycode method.
+                verifyCode(code);
+            }
+        }
+
+        // this method is called when firebase doesn't
+        // sends our OTP code due to any error or issue.
+        @Override
+        public void onVerificationFailed(FirebaseException e) {
+            // displaying error message with firebase exception.
+            Toast.makeText(Login.this, e.getMessage(), Toast.LENGTH_LONG).show();
+        }
+    };
+
+    // below method is use to verify code from Firebase.
+    private void verifyCode(String code) {
+        // below line is used for getting
+        // credentials from our verification id and code.
+        PhoneAuthCredential credential = PhoneAuthProvider.getCredential(verificationId, code);
+
+        // after getting credential we are
+        // calling sign in method.
+        signInWithCredential(credential);
+    }
+
 }
