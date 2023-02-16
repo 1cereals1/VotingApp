9f16db72f8bc3d9597d7c51f0e576d08b4cb7cb1
judxz <judcii13@gmail.com>
2023-02-16 14:30:37 +0800

    CandidateInfo

---
+++ app/src/main/java/com/example/votingapp/adaptersNlists/CandidateInfo.java
@@ -0,0 +1,16 @@
+package com.example.votingapp.adaptersNlists;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.votingapp.R;
+
+public class CandidateInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_candidate_info);
+    }
+}
