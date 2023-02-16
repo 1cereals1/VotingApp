48591b4eab2b847f170c1d91b3ea270ac918120b
GabrielTech22 <jokefer64@gmail.com>
2023-02-16 09:50:14 +0800

    UI

---
+++ app/src/main/java/com/example/votingapp/VotingGuidelines.java
@@ -0,0 +1,14 @@
+package com.example.votingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VotingGuidelines extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_voting_guidelines);
+    }
+}
