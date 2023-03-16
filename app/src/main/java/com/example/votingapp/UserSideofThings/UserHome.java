2306f2c406d387817a4c3dc1bd348411678333f9
judxz <judcii13@gmail.com>
2023-03-16 17:43:32 +0800

    awey

--- app/src/main/java/com/example/votingapp/UserSideofThings/UserHome.java
+++ app/src/main/java/com/example/votingapp/UserSideofThings/UserHome.java
@@ -4,15 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.votingapp.CandidacyForm;
+import com.example.votingapp.Login;
 import com.example.votingapp.R;
 
 public class UserHome extends AppCompatActivity {
     private Button Bvote, backButton, disableButton,gotovotepage;
+    private ImageButton Logout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,12 +26,20 @@ public class UserHome extends AppCompatActivity {
 
         Bvote = findViewById(R.id.button_vote);
         disableButton = findViewById(R.id.DisableButton);
-        gotovotepage = findViewById(R.id.DisableButton2);
+        gotovotepage  = findViewById(R.id.DisableButton2);
+        Logout = findViewById(R.id.logout);
 
         gotovotepage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(UserHome.this,VotePage.class));
+                finish();
+            }
+        });
+        Logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(UserHome.this, Login.class));
                 finish();
             }
         });
