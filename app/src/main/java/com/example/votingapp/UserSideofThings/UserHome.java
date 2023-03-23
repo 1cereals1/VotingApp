2a4bdf3da5ec44efe9f8856a7a5ef41ad00138db
Bajek0616 <118502836+Bajek0616@users.noreply.github.com>
2023-03-23 10:29:09 +0800

    wave

--- app/src/main/java/com/example/votingapp/UserSideofThings/UserHome.java
+++ app/src/main/java/com/example/votingapp/UserSideofThings/UserHome.java
@@ -27,7 +27,7 @@ import com.google.firebase.database.ValueEventListener;
 
 public class UserHome extends AppCompatActivity {
     private Button gotoresultpage, backButton, gotocandidacypage,gotovotepage;
-    private ImageButton Logout;
+    private ImageButton Logout,TOFAQS;
     private DatabaseReference myBoolean,myBoolean2,myBoolean3;
 
 
@@ -39,6 +39,7 @@ public class UserHome extends AppCompatActivity {
         gotocandidacypage = findViewById(R.id.DisableButton2);
         gotovotepage  = findViewById(R.id.DisableButton);
         Logout = findViewById(R.id.logout);
+        TOFAQS = findViewById(R.id.faqs);
 
         myBoolean = FirebaseDatabase.getInstance().getReference("Value");
         myBoolean2 = FirebaseDatabase.getInstance().getReference("Value2");
@@ -125,5 +126,14 @@ public class UserHome extends AppCompatActivity {
                 finish();
             }
         });
+
+        TOFAQS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(UserHome.this, FAQS.class));
+                finish();
+            }
+        });
+
     }
 }
